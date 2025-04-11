d0f29d392ea0bdefb8048b3ec09767f7cc1daca5
Andrei Gankin <gankin89@gmail.com>
2025-04-11 23:30:08 +0400

    Added PipeAsync method applied to a task with transforming by a sync function.

--- src/PureMonads/Pipe/PipeExtensions.cs
+++ src/PureMonads/Pipe/PipeExtensions.cs
@@ -18,7 +18,7 @@ public static class PipeExtensions
     /// <typeparam name="TValue">Value type.</typeparam>
     /// <typeparam name="TResult">Result type.</typeparam>
     /// <param name="value">The value.</param>
-    /// <param name="transform">A transforming function.</param>
+    /// <param name="transformAsync">A transforming function.</param>
     /// <returns>A task representing a result of the transforming function invocation.</returns>
     public static async Task<TResult> PipeAsync<TValue, TResult>(this TValue value, Func<TValue, Task<TResult>> transformAsync)
     {
@@ -27,12 +27,28 @@ public static class PipeExtensions
     }
 
     /// <summary>
-    /// Tranforms the value from the task by applying an async transforming function.
+    /// Tranforms the value from the task by applying a transforming function.
     /// </summary>
     /// <typeparam name="TValue">Value type.</typeparam>
     /// <typeparam name="TResult">Result type.</typeparam>
     /// <param name="task">The value task.</param>
     /// <param name="transform">A transforming function.</param>
+    /// <returns>A task representing a result of the transforming function invocation.</returns>
+    public static async Task<TResult> PipeAsync<TValue, TResult>(this Task<TValue> task, Func<TValue, TResult> transform)
+    {
+        var value = await task;
+        var result = transform(value);
+        
+        return result;
+    }
+    
+    /// <summary>
+    /// Tranforms the value from the task by applying an async transforming function.
+    /// </summary>
+    /// <typeparam name="TValue">Value type.</typeparam>
+    /// <typeparam name="TResult">Result type.</typeparam>
+    /// <param name="task">The value task.</param>
+    /// <param name="transformAsync">A transforming function.</param>
     /// <returns>A task representing a result of the transforming function invocation.</returns>
     public static async Task<TResult> PipeAsync<TValue, TResult>(this Task<TValue> task, Func<TValue, Task<TResult>> transformAsync)
     {
